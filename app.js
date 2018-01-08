a298256c1cfd7285d3323368cdfa79064a46bbb0
Chris Underwood <cunderwoodmn@gmail.com>
2018-01-07 17:28:33 -0800

    changed speed from kpm to mph

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const arrow = document.querySelector('.arrow');
 
    navigator.geolocation.watchPosition((data) => {
      console.log(data);
-     speed.textContent = data.coords.speed;
+     speed.textContent = (data.coords.speed * 0.621371);
      arrow.style.transform = `rotate(${data.coords.heading}deg)`;
    }, (err) => {
      console.error(err);
